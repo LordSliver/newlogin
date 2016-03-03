8277981819cb65d6fa55fa6521a31e8bd6c795c5
LordSliver <sliver1997@163.com>
2016-03-03 20:05:39 +0800

    try

---
+++ loginapp/app/src/main/java/com/example/y430/apptest40/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.y430.apptest40;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class MainActivity extends ActionBarActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
